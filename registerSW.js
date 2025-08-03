fa85bcd001f51438ce0e6444f4540872d31810e2
ilovepdfly <your-email@example.com>
2025-08-03 10:51:04 +0545

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ilovepdfly/sw.js', { scope: '/ilovepdfly/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
