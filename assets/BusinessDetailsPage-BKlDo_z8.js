72dba2623c80592144099d45a3cb02c67dc06c1c
ilovepdfly <ilovepdfly@gmail.com>
2025-08-27 21:57:10 +0545

    Updates

--- assets/BusinessDetailsPage-BKlDo_z8.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./main-DxKcSKLJ.js";import{P as e}from"./PlaceholderPage-RwX58kE7.js";const r=()=>s.jsx(e,{title:"Business Details"});export{r as default};
--- assets/InvoicesPage-BF2YPscd.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-DxKcSKLJ.js";import{P as o}from"./PlaceholderPage-RwX58kE7.js";const s=()=>e.jsx(o,{title:"Billing Invoices"});export{s as default};
